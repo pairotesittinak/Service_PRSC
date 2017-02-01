194d36c224cfb18218d9cdbda24e25b767fedd6a
Panupong <56030319@kmitl.ac.th>
2017-02-01 15:37:41 +0700

    UPDATE 01/02/17

--- config/express.js
+++ config/express.js
@@ -66,6 +66,14 @@ module.exports = function() {
 	app.set('views', './app/views');
 	app.set('view engine', 'hbs');
 
+
+	
+	app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
+
 	// app.set('views', './app/views');
 	// app.set('view engine', 'jade');
 
